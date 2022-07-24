0f840de8d6b5a323d584f03aa3f4967f689e152b
sake <sakep3@gmail.com>
2022-07-24 23:03:44 +0900

    mo

--- PythonSandbox/ManyCSV.py
+++ PythonSandbox/ManyCSV.py
@@ -1,8 +1,29 @@
 import csv
 import pprint
 
-with open('data/inc.txt', encoding="utf-8") as f:
- reader = csv.reader(f)
- for row in reader:
-   print(row[0])
- #print(f.read())
+import pandas as pd
+
+mylist=[]
+
+
+
+mylist.append([1, "StockNum"])
+mylist.append([1, "StockName"])
+mylist.append([1, "StockInc"])
+mylist.append([1, "StockPbr"])
+
+
+print(mylist)
+
+
+df1 = pd.DataFrame(
+    data={'Code': [10]},
+    index=["6758"]
+)
+print(df1)
+
+
+#with open('data/inc.txt', encoding="utf-8") as f:
+# reader = csv.reader(f)
+# for row in reader:
+#   print(row[0])
